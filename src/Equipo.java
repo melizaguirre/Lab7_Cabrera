110261b549e4b1025da7fbd70e2356db6f545396
username <cabreraizaguirre2002@gmail.com>
2022-03-04 15:28:21 -0600

    Clase Equipo y JFrame

---
+++ src/Equipo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BAC
+ */
+public class Equipo {
+    private String nombreEquipo;
+    private int partidosJugados;
+    private int ganados;
+    private int empatados;
+    private int perdidos;
+    private int golesFavor;
+    private int golesContra;
+    private int Diferencia;
+    private int pts;
+    
+    
+}
